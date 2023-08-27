e6c841f7e3073efb326a163f9987d14590a5a044
Albert3437 <niga2281337228@gmail.com>
2023-08-27 14:02:54 +0300

    fix bugs

--- modules/kline_collector.py
+++ modules/kline_collector.py
@@ -29,7 +29,7 @@ class BinanceData:
         for _ in range(5):
             try:
                 candles = self.client.get_klines(symbol=symbol, interval=interval, limit=limit)
-                columns = ['timestamp', 'open', 'high', 'low', 'close']
+                columns = ['timestamp', 'open', 'high', 'low', 'close', 'volume']
                 df = pd.DataFrame(candles, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_asset_volume', 'number_of_trades', 'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume', 'ignore'])
                 df = df[columns]
                 return df
