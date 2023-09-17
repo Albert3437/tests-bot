d4410fcd0c940875de7b6cbc6063e8ea0e537703
Albert3437 <niga2281337228@gmail.com>
2023-09-17 15:09:14 +0300

    fix bugs

--- modules/web_core.py
+++ modules/web_core.py
@@ -267,12 +267,14 @@ class WebCore:
         # Функция для кодироавния данных датафрейма для будущего скачивания
         return df.to_csv().encode('utf-8')
     
+
     @logging
     def get_active_deals(self):
         active_deals = []
         for strat in STRATS:
             try:
-                data = self.db.read_deals(strat)
+                db = DealsDataBase(strat)
+                data = db.read_deals(strat)
                 if data[-1]['percent'] == None:
                     active_deals.append(data[-1])
             except:
